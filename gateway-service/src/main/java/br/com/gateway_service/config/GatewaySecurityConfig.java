35ffc388b5983b1fac1d55a643f995c321920513
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2025-01-07 18:43:27 -0300

    feat: add config to authentication

---
+++ gateway-service/src/main/java/br/com/gateway_service/config/GatewaySecurityConfig.java
@@ -0,0 +1,24 @@
+package br.com.gateway_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class GatewaySecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(auth -> auth
+                        .pathMatchers("/auth-service/login").permitAll()
+                        .anyExchange().authenticated()
+                );
+
+        return http.build();
+    }
+}
